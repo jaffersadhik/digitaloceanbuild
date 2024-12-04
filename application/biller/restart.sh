059dcee7708e03cc632c54bacfaceab71a453677
Jaffer Sadhik <jaffer.sadhik@gmail.com>
2024-12-04 06:21:02 +0000

    jj

---
+++ application/biller/restart.sh
@@ -0,0 +1,73 @@
+cd subt2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd subt2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnt2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnt2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnp/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnp/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnpostlogt2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnpostlogt2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnnopayloadt2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd dnnopayloadt2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd errorlogt2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd errorlogt2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd fullmsg/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd fullmsg/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd t2e/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd t2e/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd clienthandovert2tb/low/
+docker-compose down
+docker-compose up -d
+cd ../..
+cd clienthandovert2tb/high/
+docker-compose down
+docker-compose up -d
+cd ../..
+
