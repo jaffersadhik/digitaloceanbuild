f8c24ef90050c41d3053edcc22adcbf96b3f89fc
Jaffer Sadhik <jaffer.sadhik@Agmail.com>
2024-12-09 13:58:57 +0000

    jj

--- application/dnpost/stop.sh
+++ application/dnpost/stop.sh
@@ -1,48 +1,36 @@
 cd dlrpayloaddngenerator/
 docker-compose down
-docker-compose up -d
 cd ..
 cd ac/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnpost/http/all/
 docker-compose down
-docker-compose up -d
 cd ../../..
 cd dnpost/smpp/all/
 docker-compose down
-docker-compose up -d
 cd ../../..
 cd dnpost/smpp/poller/
 docker-compose down
-docker-compose up -d
 cd ../../..
 cd duplicatecheckcleanup/
 docker-compose down
-docker-compose up -d
 cd ..
 cd kannelstatusupdator/
 docker-compose down
-docker-compose up -d
 cd ..
 cd poller/dnfp/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd poller/ifp/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd poller/scheduleblockoutpoller/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd smppconcatehandover/
 docker-compose down
-docker-compose up -d
 cd ..
-cd rc/all
+cd rc/all/
 docker-compose down
-docker-compose up -d
 cd ../..
