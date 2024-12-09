605db0caf61e9d6dd1b83ccabf4f9bbe990c9b07
Jaffer Sadhik <jaffer.sadhik@gmail.com>
2024-12-09 13:51:40 +0000

    jj

--- application/biller/stop.sh
+++ application/biller/stop.sh
@@ -1,41 +1,31 @@
 cd subt2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnt2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnt2tbbkup/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnp/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnpostlogt2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd dnnopayloadt2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd errorlogt2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd fullmsg/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd t2e/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 cd clienthandovert2tb/all/
 docker-compose down
-docker-compose up -d
 cd ../..
 
