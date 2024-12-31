fd9d28fbc887845e6b0a2546475a28f928a7272c
Jaffer Sadhik <jaffer.sadhik@gmail.com>
2024-12-31 13:15:38 +0530

    jj

---
+++ k8s/download.sh
@@ -0,0 +1,3 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubeadm"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubelet"
