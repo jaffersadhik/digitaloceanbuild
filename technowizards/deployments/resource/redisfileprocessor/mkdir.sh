2892779d8cbda11b74ade6d8f295315a75684f5e
Jaffer Sadhik <Jaffer.sadhik@gmail.com>
2025-01-08 11:38:33 +0530

    jj

---
+++ technowizards/deployments/resource/redisfileprocessor/mkdir.sh
@@ -0,0 +1,8 @@
+ mkdir -p /home/redisdata/excludegroup
+ chmod -R 777 /home/redisdata/excludegroup
+ mkdir -p /home/redisdata/excludegroup
+ chmod -R 777 /home/redisdata/excludegroup
+ mkdir -p /home/redisdata/excludegroup
+ chmod -R 777 /home/redisdata/excludegroup
+ mkdir -p /home/redisdata/excludegroup
+ chmod -R 777 /home/redisdata/excludegroup
