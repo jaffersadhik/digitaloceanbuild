4958a88bbad8d43ca1647100c92bdd3cd2937afe
Jaffer Sadhik <jaffer.sadhik@gmail.com>
2025-02-07 17:36:48 +0530

    jj

---
+++ mkdir.sh
@@ -0,0 +1,8 @@
+ mkdir -p  /home/fileprocessordata/uploadedfiles/    
+ mkdir -p  /home/fileprocessordata/splitfiles/      
+ mkdir -p  /home/fileprocessordata/campaigns/        
+ mkdir -p  /home/fileprocessordata/templates/       
+ mkdir -p  /home/fileprocessordata/groups/          
+ mkdir -p  /home/fileprocessordata/dlttemplates/     
+ mkdir -p /home/fileprocessordata/log_download/csv/ 
+
